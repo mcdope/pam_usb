093bbf078a4d66e631672ca464fd4d0c572dfc51
Tobias <tobiasbaeumer@gmail.com>
2020-12-13 13:01:56 +0100

    #24: Fuck grep, rely only on exitlevel

--- tests/can-actually-be-used/test-check-verify-created-config.sh
+++ tests/can-actually-be-used/test-check-verify-created-config.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/bash
 
 echo -e "Test:\t\t\tpamusb-check verifies the config previous tests created / reports granted"
-echo "DEBUG: pamusb-check output ->"
-pamusb-check --debug `whoami` && echo "error level 0 (success)" || echo "error level 1 (fail)"
-echo "DEBUG: pamusb-check output -> grep -> "
-pamusb-check `whoami` --debug | grep "granted" && echo "error level 0 (success)" || echo "error level 1 (fail)"
-echo "DEBUG: actual check with exit level"
-pamusb-check `whoami` 2>&1 | grep "granted" && echo -e "Result:\t\t\tPASSED!" || exit 1
+pamusb-check `whoami` 2>&1 && echo -e "Result:\t\t\tPASSED!" || exit 1
