86d8ff4d72cc8948f4afcf457942fc7fdfee3938
Tobias <tobiasbaeumer@gmail.com>
2020-12-13 12:42:33 +0100

    #24: Re-add output redir to verify test

--- tests/can-actually-be-used/test-check-verify-created-config.sh
+++ tests/can-actually-be-used/test-check-verify-created-config.sh
@@ -2,4 +2,4 @@
 
 echo -e "Test:\t\t\tpamusb-check verifies the config previous tests created / reports granted"
 pamusb-check --debug `whoami`
-pamusb-check `whoami` | grep "granted" && echo -e "Result:\t\t\tPASSED!" || exit 1
+pamusb-check `whoami` 2>&1 | grep "granted" && echo -e "Result:\t\t\tPASSED!" || exit 1
