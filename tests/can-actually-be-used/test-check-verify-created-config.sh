4970fe08d8b6e68203652b6e949cc5021e6b4682
Tobias <tobiasbaeumer@gmail.com>
2020-12-13 12:57:21 +0100

    #24: even mooore debug output ...

--- tests/can-actually-be-used/test-check-verify-created-config.sh
+++ tests/can-actually-be-used/test-check-verify-created-config.sh
@@ -2,10 +2,8 @@
 
 echo -e "Test:\t\t\tpamusb-check verifies the config previous tests created / reports granted"
 echo "DEBUG: pamusb-check output ->"
-pamusb-check --debug `whoami`
+pamusb-check --debug `whoami` && echo "error level 0 (success)" || echo "error level 1 (fail)"
 echo "DEBUG: pamusb-check output -> grep -> "
-pamusb-check `whoami` 2>&1 | grep "granted" 
-echo "DEBUG: pamusb-check output -> grep -> returnlevel check"
-pamusb-check `whoami` 2>&1 | grep "granted" && echo "error level 0 (success)" || echo "error level 0 (success)"
+pamusb-check `whoami` --debug | grep "granted" && echo "error level 0 (success)" || echo "error level 1 (fail)"
 echo "DEBUG: actual check with exit level"
 pamusb-check `whoami` 2>&1 | grep "granted" && echo -e "Result:\t\t\tPASSED!" || exit 1
