e570f4ac007ab0e1f24be077742b7c71c8f47712
Tobias <tobiasbaeumer@gmail.com>
2020-12-13 11:47:15 +0100

    #24: Gimme some output, am I supposed to guess or what? Damn actions...

--- tests/can-actually-be-used/test-check-verify-created-config.sh
+++ tests/can-actually-be-used/test-check-verify-created-config.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
 echo -e "Test:\t\t\tpamusb-check verifies the config previous tests created / reports granted"
+pamusb-check --debug `whoami`
 pamusb-check `whoami` | grep "granted" && echo -e "Result:\t\t\tPASSED!" || exit 1
