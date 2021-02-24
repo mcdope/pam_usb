82141a07ad956079decafc55c4ec614d364328a5
Tobias <tobiasbaeumer@gmail.com>
2021-02-24 21:06:25 +0100

    #51: local.c: Re-add utmp code, to be used by parent pid using process.c [WIP]

--- src/local.c
+++ src/local.c
@@ -18,6 +18,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <utmp.h>
 #include "log.h"
 #include "conf.h"
 #include "process.h"
@@ -43,8 +44,54 @@ int pusb_local_login(t_pusb_options *opts, const char *user)
 			log_error("One of the parent processes found to be a remote access daemon, denying.\n");
 			return (0);
 		}
-	}
 
-	log_debug("No remote daemons found in parent process list, seems to be local request - allowing.\n");
+		// Check for multiplexer, in case found > use utmp approach on top
+		if (strstr(name, "tmux") != NULL || strstr(name, "byobu") != NULL) {
+			log_debug("    Multiplexer detected - checking utmp...\n");
+			
+			struct utmp	utsearch;
+			struct utmp	*utent;
+			const char	*from;
+			from = ttyname(STDIN_FILENO);
+			int	i;
+
+			if (!from || !(*from))
+			{
+				log_error("        Couldn't retrieve the tty name, assuming remote\n");
+				return (0);
+			}
+
+			if (!strncmp(from, "/dev/", strlen("/dev/"))) {
+				from += strlen("/dev/");
+			}
+
+			log_debug("        TTY used: %s\n", from);
+			strncpy(utsearch.ut_line, from, sizeof(utsearch.ut_line) - 1);
+			setutent();
+			utent = getutline(&utsearch);
+			endutent();
+			
+			if (!utent)
+			{
+				log_error("No utmp entry found for tty \"%s\", assuming remote\n", from);
+				return (0);
+			}
+
+			for (i = 0; i < 4; ++i)
+			{
+				/** 
+				 * Note: despite the property name this also works for IPv4, v4 addr would be in ut_addr_v6[0] solely. 
+				 * See utmp man (http://manpages.ubuntu.com/manpages/bionic/de/man5/utmp.5.html)
+				 **/
+				if (utent->ut_addr_v6[i] != 0) 
+				{
+					log_error("        Remote authentication request: %s\n", utent->ut_host);
+					return (0);
+				}
+			}
+		}
+	}
+	
+	log_debug("No remote daemons or multiplexer sessions found in process chain, seems to be local request - allowing.\n");
 	return (1);
 }
