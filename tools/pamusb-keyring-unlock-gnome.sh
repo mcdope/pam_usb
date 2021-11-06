a3bc6c623c0228562f4e28a7a750b676ec868eec
McDope <tobiasbaeumer@gmail.com>
2021-11-06 12:09:39 +0100

    #11: Add shellscript to unlock gnome-keyring-daemon

---
+++ tools/pamusb-keyring-unlock-gnome.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if password file has correct permissions
+PERMISSIONS=`stat -c "%a %n" ~/.keyring_unlock_password | awk '{print $1}'`
+if [[ $PERMISSIONS -lt 600 || $PERMISSIONS -gt 600 ]]
+then
+    echo "Error: bad permissions on ~/.keyring_unlock_password. Please change them to 0600."
+    exit 1
+fi
+
+# Read UNLOCK_PASSWORD from ~/.keyring_unlock_password
+source ~/.keyring_unlock_password
+
+# Kill maybe autostarted daemon
+killall -q gnome-keyring-daemon
+
+# Perform unlock and start daemon
+echo -n $UNLOCK_PASSWORD | gnome-keyring-daemon --daemonize --login && gnome-keyring-daemon --start
+
+exit 0
