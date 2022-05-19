5db3a0c2968ec2a7b684fa7a5bec855801a11c65
McDope <tobiasbaeumer@gmail.com>
2022-05-19 17:02:58 +0200

    Revert "xpath.c: replace strncpy in l78"
    This reverts commit 968e7325eea376f3e05f4737b83a30e0411f334f.

--- src/xpath.c
+++ src/xpath.c
@@ -75,7 +75,7 @@ static int pusb_xpath_strip_string(char *dest, const char *src,
 	}
 
 	memset(dest, 0x0, size);
-	snprintf(dest, last_char + first_char + 1, "%s", &(src[first_char]))
+	strncpy(dest, &(src[first_char]), last_char - first_char + 1);
 	return (1);
 }
 
