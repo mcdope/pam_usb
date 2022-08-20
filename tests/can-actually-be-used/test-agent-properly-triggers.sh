6a09766cecb86dd5dc243602796873fb8a36c657
McDope <tobiasbaeumer@gmail.com>
2022-08-20 13:30:49 +0200

    #150: Use sudo for tail too

--- tests/can-actually-be-used/test-agent-properly-triggers.sh
+++ tests/can-actually-be-used/test-agent-properly-triggers.sh
@@ -11,12 +11,12 @@ sudo systemctl start pamusb-agent || exit 1
 # "unplug" virtual usb
 sudo modprobe -r g_mass_storage || exit 1
 sleep 5
-tail -n 20 /var/log/auth.log | grep "has been removed, locking down user" || exit 1
+sudo tail -n 20 /var/log/auth.log | grep "has been removed, locking down user" || exit 1
 
 # "plug" virtual usb
 sudo modprobe g_mass_storage file=./virtual_usb.img stall=0 removable=y iSerialNumber=1234567890 || exit 1
 sleep 5
-tail -n 20 /var/log/auth.log | grep "Authentication succeeded. Unlocking user" || exit 1
+sudo tail -n 20 /var/log/auth.log | grep "Authentication succeeded. Unlocking user" || exit 1
 
 # Disable agent again
 sudo systemctl stop pamusb-agent || exit 1
