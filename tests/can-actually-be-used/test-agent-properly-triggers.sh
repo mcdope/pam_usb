0c533c1508aec145cb85642cc8058054f2d2fe59
McDope <tobiasbaeumer@gmail.com>
2022-08-20 14:34:29 +0200

    #150: Wait longer after plug events

--- tests/can-actually-be-used/test-agent-properly-triggers.sh
+++ tests/can-actually-be-used/test-agent-properly-triggers.sh
@@ -11,12 +11,12 @@ sleep 5 # make sure agent is up
 
 # "unplug" virtual usb
 sudo modprobe -r g_mass_storage || exit 1
-sleep 5
+sleep 10
 sudo tail -n 100 /var/log/auth.log | grep "has been removed, locking down user" > /dev/null && echo "Lock event found" || (echo "No lock event found!" && exit 1)
 
 # "plug" virtual usb
 sudo modprobe g_mass_storage file=./virtual_usb.img stall=0 removable=y iSerialNumber=1234567890 || exit 1
-sleep 5
+sleep 10
 sudo tail -n 100 /var/log/auth.log | grep "Authentication succeeded. Unlocking user"  > /dev/null && echo "Unlock event found" || (echo "No unlock event found!" && exit 1)
 
 # Disable agent again
