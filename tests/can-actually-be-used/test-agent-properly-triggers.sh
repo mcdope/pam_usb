101b6509865c1d0420311fb260ba9a06c4f3b645
McDope <tobiasbaeumer@gmail.com>
2022-08-20 14:51:02 +0200

    #150: Come on bash, you aren't python... spaces - really?!

--- tests/can-actually-be-used/test-agent-properly-triggers.sh
+++ tests/can-actually-be-used/test-agent-properly-triggers.sh
@@ -12,12 +12,12 @@ sleep 5 # make sure agent is up
 # "unplug" virtual usb
 sudo modprobe -r g_mass_storage || exit 1
 sleep 10
-sudo tail -n 200 /var/log/auth.log | grep "pamusb-agent\[" | grep "has been removed, locking down user" > /dev/null && echo "Lock event found" || {echo "No lock event found!"; exit 1;}
+sudo tail -n 200 /var/log/auth.log | grep "pamusb-agent\[" | grep "has been removed, locking down user" > /dev/null && echo "Lock event found" || { echo "No lock event found!"; exit 1; }
 
 # "plug" virtual usb
 sudo modprobe g_mass_storage file=./virtual_usb.img stall=0 removable=y iSerialNumber=1234567890 || exit 1
 sleep 10
-sudo tail -n 200 /var/log/auth.log | grep "pamusb-agent\[" | grep "Authentication succeeded. Unlocking user"  > /dev/null && echo "Unlock event found" || {echo "No unlock event found!"; exit 1;}
+sudo tail -n 200 /var/log/auth.log | grep "pamusb-agent\[" | grep "Authentication succeeded. Unlocking user"  > /dev/null && echo "Unlock event found" || { echo "No unlock event found!"; exit 1; }
 
 # Disable agent again
 sudo systemctl stop pamusb-agent || exit 1
