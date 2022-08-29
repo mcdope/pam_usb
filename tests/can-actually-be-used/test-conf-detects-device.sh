78ef7d4ca1dd63cf30cffeee2eda12fa920b182f
McDope <tobiasbaeumer@gmail.com>
2022-08-29 18:29:05 +0200

    #31: Fix detects-device testcase

--- tests/can-actually-be-used/test-conf-detects-device.sh
+++ tests/can-actually-be-used/test-conf-detects-device.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
 echo -e "Test:\t\t\tpamusb-conf properly detects device(s)"
 echo -en "pamusb-conf output (first device):\t" # to fake the unhideable python output as expected output :P
-pamusb-conf --list-devices 2>/dev/null | grep "Linux FirstStick (1234567890)" && echo -e "Result:\t\t\tPASSED!" || exit 1
-echo -en "pamusb-conf output (first device):\t" # to fake the unhideable python output as expected output :P
-pamusb-conf --list-devices 2>/dev/null | grep "Linux SecondStick (1234567891)" && echo -e "Result:\t\t\tPASSED!" || exit 1
+pamusb-conf --list-devices 2>/dev/null | grep "Linux File-Stor Gadget (1234567890)" && echo -e "Result:\t\t\tPASSED!" || exit 1
