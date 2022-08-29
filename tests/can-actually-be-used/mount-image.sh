7571f19813b41f1069dc5f9393654c1fe90ea78f
McDope <tobiasbaeumer@gmail.com>
2022-08-29 18:00:28 +0200

    #31: Adjust tests for multi-device support

--- tests/can-actually-be-used/mount-image.sh
+++ tests/can-actually-be-used/mount-image.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Load module with image
-sudo modprobe g_mass_storage file=./virtual_usb.img stall=0 removable=y iSerialNumber=1234567890
+sudo modprobe g_mass_storage file=./virtual_usb.img stall=0 removable=y iSerialNumber=1234567890 iProduct=FirstStick
 echo "Info: sleeping 5s to ensure kernel picks up our new device..."
 sleep 5
 
@@ -19,3 +19,19 @@ sudo mkfs.vfat "/dev/"$CREATED_DEVICE"1"
 mkdir -p /tmp/fakestick
 sudo mount -t vfat "/dev/"$CREATED_DEVICE"1" /tmp/fakestick -o rw,umask=0000
 
+# Load module with second image
+sudo modprobe g_mass_storage file=./virtual_usb_alt.img stall=0 removable=y iSerialNumber=1234567891 iProduct=SecondStick
+echo "Info: sleeping 5s to ensure kernel picks up our new device..."
+sleep 5
+
+# Determine device id, create partition and format it
+CREATED_DEVICE_2=$(lsblk | grep 16M | awk '{ print $1 }')
+echo "Info: fake device registered as /dev/$CREATED_DEVICE_2"
+echo "Info: creating partition..."
+echo 'type=83' | sudo sfdisk /dev/$CREATED_DEVICE_2
+echo "Info: formatting partition as vfat..."
+sudo mkfs.vfat "/dev/"$CREATED_DEVICE_2"1"
+
+# Create mountpoint and mount alt fake stick
+mkdir -p /tmp/fakestick_alt
+sudo mount -t vfat "/dev/"$CREATED_DEVICE_2"1" /tmp/fakestick_alt -o rw,umask=0000
--- tests/can-actually-be-used/run-tests.sh
+++ tests/can-actually-be-used/run-tests.sh
@@ -7,8 +7,8 @@ rm -rf /home/`whoami`/.pamusb
 
 # Run tests
 ./test-conf-detects-device.sh && \
-./test-conf-adds-device.sh && \
+./test-conf-adds-devices.sh && \
 ./test-conf-adds-user.sh && \
-./test-conf-doesnt-add-user-twice.sh && \
+./test-conf-doesnt-add-user-twice-but-adds-a-second-device.sh && \
 ./test-check-verify-created-config.sh && \
 ./test-agent-properly-triggers.sh
