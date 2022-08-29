d71b610d76ea02d00a6f2fe87344070a3582c114
McDope <tobiasbaeumer@gmail.com>
2022-08-29 18:14:36 +0200

    #31: We cant load the mod 2x, so adjust tests accordingly

---
+++ tests/can-actually-be-used/test-conf-doesnt-add-user-twice-but-adds-a-second-device.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+echo -e "Test:\t\t\tpamusb-conf doesn't add user(s) twice, but it adds a second device for an existing user"
+
+# "unplug" virtual usb
+sync && sync && sync
+sudo umount /tmp/fakestick
+sudo modprobe -r g_mass_storage || exit 1
+sleep 10
+
+# "plug" another virtual usb
+sudo modprobe g_mass_storage file=./virtual_usb.img stall=0 removable=y iSerialNumber=1234567891 iProduct=SecondStick || exit 1
+sleep 10
+
+echo -en "pamusb-conf --add-device output:\t" # to fake the unhideable python output as expected output :P
+sudo pamusb-conf --add-device=test2 --device=0 --volume=0 --yes | grep "Done" && cat /etc/security/pam_usb.conf | grep "SecondStick" > /dev/null && echo -e "Result:\t\t\tPASSED!" || exit 1
+
+echo -en "pamusb-conf --add-user output:\t" # to fake the unhideable python output as expected output :P
+sudo pamusb-conf --add-user=`whoami` --device=0 --yes | grep "Done" && cat /etc/security/pam_usb.conf | grep "<device>SecondStick</device>" > /dev/null && echo -e "Result:\t\t\tPASSED!" || exit 1
--- tests/can-actually-be-used/umount-image.sh
+++ tests/can-actually-be-used/umount-image.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/bash
 sync && sync && sync
 sudo umount /tmp/fakestick
-sudo umount /tmp/fakestick_alt
 sudo modprobe -r g_mass_storage
 
 rm virtual_usb.img
-rm virtual_usb_alt.img
 rm -rf /tmp/fakestick
-rm -rf /tmp/fakestick_alt
 rm -rf /home/`whoami`/.pamusb
