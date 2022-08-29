68504d78bb336a51a939944c1e550af21ac06ac8
McDope <tobiasbaeumer@gmail.com>
2022-08-29 19:04:12 +0200

    #31: Get some debug output

--- tests/can-actually-be-used/test-check-verify-created-config.sh
+++ tests/can-actually-be-used/test-check-verify-created-config.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 echo -e "Test:\t\t\tpamusb-check verifies the config previous tests created / reports granted"
-pamusb-check `whoami` 2>&1 && echo -e "Result:\t\t\tPASSED!" || exit 1
+pamusb-check --debug `whoami` && echo -e "Result:\t\t\tPASSED!" || exit 1
