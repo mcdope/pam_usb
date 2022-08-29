3ab225056382f203bc27243f00af1af76e2e649f
McDope <tobiasbaeumer@gmail.com>
2022-08-29 18:16:51 +0200

    #31: Rename add-device back, since it only adds a single device

--- tests/can-actually-be-used/run-tests.sh
+++ tests/can-actually-be-used/run-tests.sh
@@ -7,7 +7,7 @@ rm -rf /home/`whoami`/.pamusb
 
 # Run tests
 ./test-conf-detects-device.sh && \
-./test-conf-adds-devices.sh && \
+./test-conf-adds-device.sh && \
 ./test-conf-adds-user.sh && \
 ./test-conf-doesnt-add-user-twice-but-adds-a-second-device.sh && \
 ./test-check-verify-created-config.sh && \
